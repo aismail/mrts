a7ef30b09c61cd20a79157fff46237521dc57d7e
Cristi Barca <cristi.barca@gmail.com>
2012-04-03 00:51:05 +0300

    AlgSerTests.Rprop: Setting the project - src added

---
+++ AlgSerTests/Rprop/src/network/AbstractNode.java
@@ -0,0 +1,117 @@
+package network;
+
+/**
+ * Abstract parent for nodes
+ */
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Abstract parent for nodes
+ * 
+ * @author gsc
+ * @author cbarca
+ */
+public abstract class AbstractNode extends AbstractArcNode {
+    
+    /**
+     * Return node error term
+     * @param node error term
+     */
+    public double getError() {
+    	return(error);
+    }
+    
+    /**
+     * Define node error term
+     * @param arg new error term
+     */
+    public void setError(double arg) {
+    	error = arg;
+    }
+    
+    /**
+     * Return node value
+     * @return node value
+     */
+    public double getValue() {
+    	return(value);
+    }
+    
+    /**
+     * Define node value
+     * @param arg new node value
+     */
+    public void setValue(double arg) {
+    	value = arg;
+    }
+    
+    /**
+     * Connect to another node via an arc
+     * @param destination node
+     * @param arc to connect with
+     */
+    public void connect(AbstractNode destination, Arc arc) {
+    	output_arcs.add(arc);
+	
+    	destination.input_arcs.add(arc);
+	
+    	arc.setInputNode(this);
+    	arc.setOutputNode(destination);
+    }
+    
+    /**
+     * What am I connected to?
+     */
+    public String dumpConnections() {
+    	String result = "id:" + id;
+	
+    	result += ":input:";
+    	Iterator<Arc> ii = input_arcs.iterator();
+    	while (ii.hasNext()) {
+    		result += ii.next().toString() + ":";
+    	}
+	
+    	result += "output:";
+    	ii = output_arcs.iterator();
+    	while (ii.hasNext()) {
+    		result += ii.next().toString() + ":";
+    	}
+	
+    	return(result);
+    }
+    
+    /**
+     * Return object state as a string
+     * @return object state as a string
+     */
+    public String toString() {
+    	return(id + " error:" + error + " value:" + value + " input:" 
+    			+ input_arcs.size() + " output:" + output_arcs.size());
+    }
+    
+    /**
+     * Error for this node
+     */
+    double error;
+    
+    /**
+     * Value for this node
+     */
+    double value;
+    
+    /**
+     * Input arcs
+     */
+    ArrayList<Arc> input_arcs = new ArrayList<Arc>();
+    
+    /**
+     * Output arcs
+     */
+    ArrayList<Arc> output_arcs = new ArrayList<Arc>();	
+    
+    /**
+	 * Eclipse generated 
+	 */
+	private static final long serialVersionUID = -8437051058370670157L;
+}
---
+++ AlgSerTests/Rprop/src/network/SequenceGenerator.java
@@ -0,0 +1,28 @@
+package network;
+
+/**
+ * Provide unique ID for arcs/nodes
+ */
+
+/**
+ * Provide unique ID for arcs/nodes
+ * 
+ * @author gsc
+ * @author cbarca
+ */
+public class SequenceGenerator {
+
+    /**
+     * Each arc/node has a unique identifer. 
+     * This was to help w/development, but it doesn't hurt to retain.
+     * @return unique identifier
+     */
+    public static synchronized int getId() {
+    	return(_id++);
+    }
+    
+    /**
+     * Contains the next Arc/Node identifier.
+     */
+    private static int _id = 1;
+}
