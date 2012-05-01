5224c3904c8cea429a66d05ac9580dcbabe953ac
Cristi Barca <cristi.barca@gmail.com>
2012-05-01 23:31:41 +0300

    Comment added to IConnector

--- mrts/src/cassdb/interfaces/IConnector.java
+++ mrts/src/cassdb/interfaces/IConnector.java
@@ -2,6 +2,11 @@ package cassdb.interfaces;
 
 import me.prettyprint.hector.api.Keyspace;
 
+/**
+ * Interface for cassandra database Connector
+ * 
+ * @author cbarca
+ */
 public interface IConnector {
 	void createSchema(String keyspaceName);
 	Keyspace getKeyspace();
