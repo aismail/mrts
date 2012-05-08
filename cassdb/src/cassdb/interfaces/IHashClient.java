ef6436edbda7e197203a5207a4ccbe843b219d14
Cristi Barca <cristi.barca@gmail.com>
2012-05-09 00:28:34 +0300

    Cassdb: interfaces updated - new operations added

--- cassdb/src/cassdb/interfaces/IHashClient.java
+++ cassdb/src/cassdb/interfaces/IHashClient.java
@@ -1,5 +1,7 @@
 package cassdb.interfaces;
 
+import java.util.List;
+
 /**
  * Interface for Hash Client
  * 
@@ -8,10 +10,13 @@ package cassdb.interfaces;
 public interface IHashClient {
 	// GET methods
 	public Object get(String colfamName, Integer keyL, Integer keyC);
+	public Object get(String colfamName, String keyL, Long keyC);
 	public Object get(String colfamName, String keyL, String keyC);
+	public List<Object> get(String colfamName, String keyL, Long k1, Long k2, int maxc);
 	
 	// PUT methods
 	public void put(String colfamName, Integer keyL, Integer keyC, Object value);
+	public void put(String colfamName, String keyL, Long keyC, Object value);
 	public void put(String colfamName, String keyL, String keyC, Object value);
 	public void startBatchPut();
 	public void batchPut(String colfamName, Integer keyL, Integer keyC, Object value);
@@ -19,5 +24,6 @@ public interface IHashClient {
 	
 	// REMOVE methods
 	public void remove(String colfamName, Integer keyL, Integer keyC);
-	public void remove(String colfamName, String keyL, String keyC);
+	public void remove(String colfamName, String keyL, Long keyC);
+	public void remove(String colfamName, String keyL, String keyC);	
 }
