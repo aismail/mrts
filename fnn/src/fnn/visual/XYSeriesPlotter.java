22814faf629fe274882fb316734cc4cbb34b8f8f
Cristi Barca <cristi.barca@gmail.com>
2012-05-27 14:05:16 +0300

    Fnn: Plotter renamed to XYSeriesPlotter + refactored, using new chart lib

---
+++ fnn/src/fnn/visual/XYSeriesPlotter.java
@@ -0,0 +1,68 @@
+package fnn.visual;
+
+import java.awt.Dimension;
+import java.util.HashMap;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class XYSeriesPlotter implements IPlotter {
+	private double[] _buff_xvals, _buff_yvals;
+	private String _title;
+	private int _buff_size = 0, _max_buff_size = 0;
+	private JFreeChart _chart;
+	private ChartPanel _chart_panel;
+	private XYSeriesCollection _data;
+	private HashMap<String, XYSeries> _hseries;
+	
+	public XYSeriesPlotter(String title, int max_buff_size) {
+		_max_buff_size = max_buff_size;
+		_buff_xvals = new double[_max_buff_size];
+		_buff_yvals = new double[_max_buff_size];
+		_title = title;
+		_data = new XYSeriesCollection();
+		_hseries = new HashMap<String, XYSeries>();
+		_chart = ChartFactory.createXYLineChart(
+				_title, 
+				"X", 
+				"Y", 
+				_data, 
+				PlotOrientation.VERTICAL, 
+				true, true, false);
+		_chart_panel = new ChartPanel(_chart);
+		_chart_panel.setPreferredSize(new Dimension(800, 600));
+	}
+	
+	@Override
+	public void addSeries(String name) {
+		XYSeries series = new XYSeries(name);
+		_data.addSeries(series);
+		_hseries.put(name, series);
+	}
+
+	@Override
+	public void addXYValue(String series, double x, double y) {
+		_buff_xvals[_buff_size] = x;
+		_buff_yvals[_buff_size] = y;
+		_buff_size++;
+		
+		if (_buff_size == _max_buff_size) {
+			for (int i = 0; i < _buff_xvals.length; i++) {
+				_hseries.get(series).add(_buff_xvals[i], _buff_yvals[i]);
+			}
+			
+			_buff_size = 0;
+			
+			return;
+		}
+	}
+	
+	@Override
+	public ChartPanel getChartPanel() {
+		return _chart_panel;
+	}
+}
