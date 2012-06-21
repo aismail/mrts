ea646603efcf43982c14ba8e8cae88aa655f78a7
Cristi Barca <cristi.barca@gmail.com>
2012-06-22 00:06:03 +0300

    Mrts-io: adding rprop neural network export support

--- mrts-io/src/mrtsio/mexport/Exporter.java
+++ mrts-io/src/mrtsio/mexport/Exporter.java
@@ -1,5 +1,65 @@
 package mrtsio.mexport;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import mrtsio.dbconx.MrtsConnector;
+import cassdb.interfaces.IHashClient;
+import cassdb.internal.HashClient;
+import fnn.network.AbstractArc;
+import fnn.network.AbstractNode;
+import fnn.network.RpropNetwork;
+import fnn.util.RunParams;
+
+/**
+ * Static class exporter - export the trained neural network
+ * from cassandra, in different formats
+ * 
+ * @author cbarca
+ */
 public class Exporter {
 
+	/**
+	 * Export from cassandra the trained rprop network
+	 * @param runParams run parameters object
+	 * @return resilient backprop network (trained one)
+	 */
+	public static RpropNetwork exportRpropNeuralNet(RunParams runParams) {
+		MrtsConnector conx = new MrtsConnector();
+		IHashClient hash = new HashClient(conx.getKeyspace());
+		Map<Integer, Object> map = new HashMap<Integer, Object>();
+		
+		// Create Resilient Backprop neural network
+		RpropNetwork network = new RpropNetwork(runParams.getNetStruct());
+		
+		// Pull neural net from cassandra
+		
+		// Pull the first layer of weights (input layer) 
+		for (AbstractNode node : network.getInputNodes()) {
+			map = hash.getRow(MrtsConnector.NET_SER_COLFAM, 
+					node.getId(), 
+					node.getOutputArcs().size());
+			
+			for (AbstractArc arc : node.getOutputArcs()) {
+				Double w = (Double)map.get(arc.getOutputNode().getId());
+				arc.setWeight(w);	
+			}
+		}
+		
+		// Pull the rest of the weights (middle layers)
+		for (AbstractNode[] nodes : network.getMiddleLayers()) {
+			for (AbstractNode node : nodes) {
+				map = hash.getRow(MrtsConnector.NET_SER_COLFAM, 
+						node.getId(), 
+						node.getOutputArcs().size());
+				
+				for (AbstractArc arc : node.getOutputArcs()) {
+					Double w = (Double)map.get(arc.getOutputNode().getId());
+					arc.setWeight(w);	
+				}
+			}
+		}
+		
+		return network;
+	}
 }
