bfe4ca48d8d6af6cdf6cb96ce0059236c3ea906e
Cristi Barca <cristi.barca@gmail.com>
2012-06-22 00:38:47 +0300

    Fnn: plotter interface updated - getting the chart plot (for customization)

--- fnn/src/fnn/visual/IPlotter.java
+++ fnn/src/fnn/visual/IPlotter.java
@@ -1,9 +1,11 @@
 package fnn.visual;
 
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.plot.XYPlot;
 
 public interface IPlotter {
 	public void addXYValue(String series, double x, double y);
 	public void addSeries(String name);
 	public ChartPanel getChartPanel();
+	public XYPlot getChartPlot();
 }
