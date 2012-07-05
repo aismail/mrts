4f9884559f0865ae9d63a2d2ead6b520c51dda8f
Cristi Barca <cristi.barca@gmail.com>
2012-07-06 00:21:47 +0300

    Mrts-client: ArrhyRprop demo trainer

---
+++ mrts-client/src/neuralnet/arrhy/ArrhyRprop.java
@@ -0,0 +1,52 @@
+package neuralnet.arrhy;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import neuralnet.IClassifier;
+
+import fnn.network.Rprop;
+import fnn.network.RpropNetwork;
+
+/**
+ * DemoRprop class - Resilient Backpropagation 
+ * 
+ * @author cbarca
+ */
+public class ArrhyRprop extends Rprop implements IClassifier {
+
+    /**
+     * Constructor for new Resilient Backpropagation network
+     * @param network Rprop network 
+     */
+    public ArrhyRprop(RpropNetwork network) {
+    	super(network);
+    }
+
+    /**
+     * Constructor for existing Rprop network
+     * @param file serialized Network memento
+     */
+    public ArrhyRprop(File file) throws IOException, 
+    	FileNotFoundException, ClassNotFoundException {
+    	super(file);
+    }
+
+    /**
+     * Classifier for network's output
+     * @param input network test data input
+     * @return the network output value
+     */
+	public int classifier(double[] input) {
+		double[] output = runNetwork(input);
+
+		if (output[0] > 0.9 && output[1] < 0.1) {
+			return (1);
+		} else if (output[0] < 0.1 && output[1] > 0.9) {
+			return (-1);
+		}
+
+		return(0);
+	}
+}
